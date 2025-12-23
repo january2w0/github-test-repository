89e5d171271d5e1a9c2b94779903fae3349856f8
Lee Wooyoung <a84265507@gmail.com>
2025-12-24 01:33:33 +0900

    내 생애 첫 커밋

---
+++ first/first/main.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello, World!\n");
+    int a = 10;
+    int b = 20;
+    printf("a + b = %d\n", a + b);
+    return 0;
+}
