bf6669d05a954ba6d4819f9b8b69f5c97b493c98
Lee Wooyoung <a84265507@gmail.com>
2025-12-24 01:43:15 +0900

    두번쨰 커밋

--- first/first/main.c
+++ first/first/main.c
@@ -7,5 +7,8 @@ int main(void) {
     int a = 10;
     int b = 20;
     printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
     return 0;
 }
